44ca0534bf8a51156d62e6ab078b9439a8baeb7d
Monika sanoria <sanoriamonika@gmail.com>
2025-11-22 12:26:54 -0800

    Deploy: 2025-11-22 12:26:54

--- dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Kill any existing Jekyll processes
-pkill -f jekyll 2>/dev/null
-
-# Start Jekyll on port 4000 with livereload
-bundle exec jekyll serve --port 4000 --livereload --drafts --incremental
-
-EOF
